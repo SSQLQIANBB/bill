baa2ad86699da2735ac093e4058774ac37441aff
SSQLQIANBB <1215323732@qq.com>
2026-06-10 23:47:39 +0800

    change session.py

--- services/api/app/db/session.py
+++ services/api/app/db/session.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.sqlalchemy_database_url, pool_pre_ping=True)
+engine = create_async_engine(settings.sqlalchemy_database_url)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
